51e0b15764d3a2980bf140362cb6b0aa3c122feb
JeanCarloCorso <jeancarlocorso@hotmail.com>
2017-10-10 10:54:48 -0300

    Migrações do bamco de dados caixa-farmacia

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,23 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'jeroennoten/laravel-adminlte' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'JeroenNoten\\LaravelAdminLte\\ServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+);
---
+++ database/migrations/2017_10_04_000004_create_usuarios_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $set_schema_table = 'usuarios';
+
+    /**
+     * Run the migrations.
+     * @table usuarios
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('idusuarios');
+            $table->string('login', 45)->nullable();
+            $table->string('senha', 45)->nullable();
+            $table->unsignedInteger('vendedores_idvendedores');
+
+            $table->index(["vendedores_idvendedores"], 'fk_usuarios_vendedores1_idx');
+
+
+            $table->foreign('vendedores_idvendedores', 'fk_usuarios_vendedores1_idx')
+                ->references('idvendedores')->on('vendedores')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->set_schema_table);
+     }
+}
